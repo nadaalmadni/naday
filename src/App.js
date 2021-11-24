7407d7cf93f65bf5eee54ab6e93c5a88f0147747
nadaalmadni <nada.abdelraziq@gmail.com>
2021-11-24 16:09:56 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import snow from "./snow.jpg"
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <img className='bosha' src={snow} alt="snow"/> 
+      <h1 className='title red'>HELLO</h1>
+      <h2 className='title red'>MARIAM :)</h2>
+      <img className='nada' src='/snoww.jpeg' alt='public'/>
+     <br/>
+     <br/>
+     <br/>
+     <iframe width="1003" height="564" src="https://www.youtube.com/embed/UrYCgqBK_CQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     </div>
+   
   );
+ 
 }
 
 export default App;
